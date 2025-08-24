979afd7da9318d0844c90f08cf70ac5fd18a7416
Oscar Melo <oskr975@gmail.com>
2025-08-24 16:55:09 -0500

    Se agrega validacion de campos name, lastname, email, salary

--- domain/usecase/src/main/java/co/com/pragma/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/pragma/usecase/user/UserUseCase.java
@@ -7,6 +7,10 @@ import org.slf4j.Logger;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 @RequiredArgsConstructor
 public class UserUseCase{
@@ -23,15 +27,30 @@ public class UserUseCase{
     }
 
     public Mono<User> registerUser(User user){
+
+        String errorValidationFields = Stream.<String>builder()
+                .add(user.getName() == null || user.getName().isEmpty() ? "name" : null)
+                .add(user.getLastName() == null || user.getLastName().isEmpty() ? "lastName" : null)
+                .add(user.getEmail() == null || user.getEmail().isEmpty() ? "email" : null)
+                .add(user.getSalary() == null ? "salary" : null)
+                .build()
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(", "));
+        if (!errorValidationFields.isEmpty()) {
+            return Mono.error(new RuntimeException("Error: Campo(s) faltante(s): " + errorValidationFields + "."));
+        }
+        else{
         return userRepository.RegisterUser(user).doOnSubscribe(subscription -> log.info("microservice create user init")).doOnNext(User -> log.info("{} User created correctly", user.getName())).onErrorResume(
                 throwable -> {
                     if (throwable instanceof Exception) {
                         log.error("The email: {} is already used", user.getEmail(), throwable);
                         return Mono.error(new RuntimeException("Error: The email is already used."));
-                    } else {
+                    }
+                    else {
                         log.error("Internal Error: {}", throwable.getMessage(), throwable);
                         return Mono.error(new RuntimeException("Error creating user. Please try again later.."));
                     }
                 });
+        }
     }
 }
